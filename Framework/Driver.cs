4b144ac1f71ca1318a850d09163b5a62986c85f8
Evelina D <evelinadiksaite@gmail.com>
2023-11-15 21:01:13 +0200

    Updated Driver.cs file and installed required packages

--- Framework/Driver.cs
+++ Framework/Driver.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Framework
 {
     internal class Driver
     {
+        public static IWebDriver driver;
+
+        public static void InitializeMaximizedDriver()
+        {
+            ChromeOptions MyOptions = new ChromeOptions();
+            MyOptions.AddArgument("--start-maximized");
+            driver = new ChromeDriver(MyOptions);
+        }
+
+        internal static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        public static void OpenPage(string url)
+        {
+            driver.Url = url;
+        }
+
+        public static string GetPageTitle()
+        {
+            return driver.Title;
+        }
+
+        public static string GetCurrentUrl()
+        {
+            return driver.Url;
+        }
+
+        public static void QuitDriver()
+        {
+            driver.Quit();
+        }
     }
 }
+
