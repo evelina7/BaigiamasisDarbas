75e48b7e794473fc416d894706ec29c3ecd0826d
Evelina D <evelinadiksaite@gmail.com>
2023-11-16 21:23:06 +0200

    Updated Common file

--- Framework/Pages/Common.cs
+++ Framework/Pages/Common.cs
@@ -1,16 +1,23 @@
 ﻿using OpenQA.Selenium;
 
 namespace Framework.Pages
+
 {
-    internal class Common
+    public class Common
     {
         internal static IWebElement GetElement(string locator)
         {
             return Driver.GetDriver().FindElement(By.XPath(locator));
         }
+
         internal static void ClickElement(string locator)
         {
             GetElement(locator).Click();
+        }
+
+        internal static void SendKeysToElement(string locator, string keys)
+        {
+            GetElement(locator).SendKeys(keys);
         }
     }
 }
