3f9b152a752c7be67af343210db71d42d4e2e062
Evelina D <evelinadiksaite@gmail.com>
2023-11-20 19:37:46 +0200

    Added product comparison test scenario

---
+++ Framework/Pages/TeliaPrekes.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Pages
+{
+    public class TeliaPrekes
+    {
+        private static string firstProduct = "//*[@class='js-product-compare-image']";
+        private static string firstProductComparisonCheckbox = "//*[@class='col-lg-4 col-sm-6 col-xs-12 ']//*[text()='Palyginti']";  
+        private static string secondProductComparisonCheckbox = "(//*[@class='col-lg-4 col-sm-6 col-xs-12 ']//*[text()='Palyginti'])[2]";
+        private static string productComparisonButton = "//*[@class='sticky-navigation-footer__button']";
+        private static string openProductComparisonPageButton = "//*[@href='/palyginimas/prekes/mobilieji-telefonai']";
+        private static string comparisonModalProduct_1 = "(//*[@class='compare-basket']//*[@class='col-sm-4'])[1]";
+        private static string comparisonModalProduct_2 = "(//*[@class='compare-basket']//*[@class='col-sm-4'])[2]";
+        private static string comparisonPageProduct_1 = "(//*[@class='product-compare__wrapper']//*[@class='col-lg-3 col-sm-4'])[1]";
+        private static string comparisonPageProduct_2 = "(//*[@class='product-compare__wrapper']//*[@class='col-lg-3 col-sm-4'])[2]";
+
+        public static void ClickOnFirstProduct()
+        {
+            Common.ClickElement(firstProduct);
+        }
+
+        public static void SelectFirstProductComparisonCheckbox()
+        {
+            Common.ClickElement(firstProductComparisonCheckbox);
+        }
+
+        public static void SelectSecondProductComparisonCheckbox()
+        {
+            Common.ClickElement(secondProductComparisonCheckbox);
+        }
+
+        public static void ClickOnProductComparisonButton()
+        {
+            Common.WaitForElementToBeVisible(productComparisonButton);
+            Common.ClickElement(productComparisonButton);
+        }
+
+        public static string GetTitleFromProductComparisonModal_1()
+        {
+            string element = Common.GetElementAttribute(comparisonModalProduct_1, "value");
+            return element;
+        }
+
+        public static string GetTitleFromProductComparisonModal_2()
+        {
+            string element = Common.GetElementAttribute(comparisonModalProduct_2, "value");
+            return element;
+        }
+
+        public static void ClickOnButtonToOpenProductComparisonPage()
+        {
+            Common.WaitForElementToBeVisible(openProductComparisonPageButton);
+            Common.ClickElement(openProductComparisonPageButton);
+        }
+
+        public static string GetComparedProductTile_1()
+        {
+            string element = Common.GetElementAttribute(comparisonPageProduct_1, "value");
+            return element;
+        }
+
+        public static string GetComparedProductTile_2()
+        {
+            string element = Common.GetElementAttribute(comparisonPageProduct_2, "value");
+            return element;
+        }
+
+
+    }
+}
---
+++ Tests/ProductComparison.cs
@@ -0,0 +1,34 @@
+﻿using Framework.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class ProductComparison : BaseTests
+    {
+        [Test]
+        public void CompareTwoMobilePhones()
+        {
+            Telia.ClickEshopSelectionButton();
+            Telia.SelectPhonesFromEshopSelection();
+            TeliaPrekes.SelectFirstProductComparisonCheckbox();
+            TeliaPrekes.SelectSecondProductComparisonCheckbox();
+            TeliaPrekes.ClickOnProductComparisonButton();
+
+            string productFromComparisonModal_1 = TeliaPrekes.GetTitleFromProductComparisonModal_1();
+            string productFromComparisonModal_2 = TeliaPrekes.GetTitleFromProductComparisonModal_2();
+
+            TeliaPrekes.ClickOnButtonToOpenProductComparisonPage();
+
+            string productFromComparisonPage_1 = TeliaPrekes.GetComparedProductTile_1();
+            string productFromComparisonPage_2 = TeliaPrekes.GetComparedProductTile_2();
+
+            Assert.That(productFromComparisonPage_1, Is.EqualTo(productFromComparisonModal_1));
+            Assert.That(productFromComparisonPage_2, Is.EqualTo(productFromComparisonModal_2));
+        }
+    }
+}
